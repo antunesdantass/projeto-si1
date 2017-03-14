a3fc44c845d33cc384cdabbd7eaff0c4528a2de6
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2017-03-14 16:12:23 -0300

    navbar, rotas adicionadas e css adicionados

--- front-end/js/routes.js
+++ front-end/js/routes.js
@@ -8,6 +8,14 @@ angular.module('adExtreme').config(function ($routeProvider, $locationProvider) 
             templateUrl: 'views/company/company_home.html',
             controller: 'CompanyController'
         })
+        .when('/login', {
+            templateUrl: 'views/autenticacao/login.html',
+            controller: 'LoginController'
+        })
+        .when('/cadastro', {
+            templateUrl: 'views/autenticacao/cadastro.html',
+            controller: 'CadastroController'
+        })
         .otherwise({
             redirectTo: '/'
         });
